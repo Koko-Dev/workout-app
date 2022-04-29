7a28658b17c2c572c0cdd38e2bc0d19fb714a369
Koko-Dev <koko.webdev@gmail.com>
2022-04-29 17:59:30 -0400

    In script.js: Put call to geolocation in a conditional which first determines if navigator.gelocation is available;  on success, destructure longitude and latitude from position.coords and log to the console; also, add the css for the project - these will be modified as we go

--- js/script.js
+++ js/script.js
@@ -21,11 +21,15 @@ const inputElevation = document.querySelector('.form__input--elevation');
 				getCurrentPosition(success, error)
 				getCurrentPosition(success, error, options)
 */
+if (navigator.geolocation) {
+	navigator
+		.geolocation
+		.getCurrentPosition(function (position) {
+			const {latitude} = position.coords;
+			const {longitude} = position.coords;
+			console.log(latitude, longitude);
 
-navigator
-	.geolocation
-	.getCurrentPosition(function (position) {
-		console.log(position);
-	}, function () {
-		alert('Could not get your position');
-	})
+		}, function () {
+			alert('Could not get your position');
+		})
+}
