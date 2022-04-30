bd2b7ba02f7a34883926af117346f5f72085c43a
Koko-Dev <koko.webdev@gmail.com>
2022-04-30 11:54:35 -0400

    In script.js, add class App and list the methods we think the app will need for internal workings; Refactor
    // class App
    
    class App {
        constructor() {
    
        }
    
        // Get Position
        _getPosition() {
            console.log('_getPosition');
        }
    
        // Load Map
        _loadMap() {
            console.log('Load Map');
        }
    
        // Show Form
        _showForm() {
            console.log('Show Form');
        }
    
        // Hide Form
        _hideForm() {
            console.log('Hide Form');
        }
    
        // Toggle Elevation Field
        _toggleElevationField() {
            console.log('Toggle Elev');
        }
    
        // New Workout
        _newWorkout() {
            console.log('New Workout');
        }
    
        // Render Workout Marker
        _renderWorkoutMarker() {
            console.log('Render Workout Marker');
        }
    
        // Render Workout
        _renderWorkout() {
            console.log('Render Workout');
        }
    
        // Move to Popup
        _moveToPopup() {
            console.log('Move to Popup');
        }
    
        // Set Local Storage
        _setLocalStorage() {
            console.log('Set Local Storage');
        }
    
        // Get Local Storage
        _getLocalStorage() {
            console.log('Get Local Storage');
        }
    
        // Reset
        reset() {
            console.log('Reset');
        }
    }

--- js/script.js
+++ js/script.js
@@ -13,21 +13,89 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 
 /*
-	Note:  Geolocation.getCurrentPosition syntax
-	 The Geolocation.getCurrentPosition() method is used to get
-	  the current position of the device
-		 code:
-		    getCurrentPosition(success)
-				getCurrentPosition(success, error)
-				getCurrentPosition(success, error, options)
+ Note:  Geolocation.getCurrentPosition syntax
+ The Geolocation.getCurrentPosition() method is used to get
+ the current position of the device
+ code:
+ getCurrentPosition(success)
+ getCurrentPosition(success, error)
+ getCurrentPosition(success, error, options)
 
-	Note: From google maps: https://www.google.com/maps/place/Virginia/@37.9820815,-81.6639165,7z/data=!3m1!4b1!4m5!3m4!1s0x884cd670bdbcb2cd:0xc04e4149b746a695!8m2!3d37.4315734!4d-78.6568942
+ Note: From google maps: https://www.google.com/maps/place/Virginia/@37.9820815,-81.6639165,7z/data=!3m1!4b1!4m5!3m4!1s0x884cd670bdbcb2cd:0xc04e4149b746a695!8m2!3d37.4315734!4d-78.6568942
 
  https://www.google.com/maps/@37.0722523,-76.3753041,15z
-*/
+ */
 
 // Todo:  Reassign leaflet's map variable  and the mapEvent to the global scope
 let map, mapEvent;
+
+class App {
+	constructor() {
+
+	}
+
+	// Get Position
+	_getPosition() {
+		console.log('_getPosition');
+	}
+
+	// Load Map
+	_loadMap() {
+		console.log('Load Map');
+	}
+
+	// Show Form
+	_showForm() {
+		console.log('Show Form');
+	}
+
+	// Hide Form
+	_hideForm() {
+		console.log('Hide Form');
+	}
+
+	// Toggle Elevation Field
+	_toggleElevationField() {
+		console.log('Toggle Elev');
+	}
+
+	// New Workout
+	_newWorkout() {
+		console.log('New Workout');
+	}
+
+	// Render Workout Marker
+	_renderWorkoutMarker() {
+		console.log('Render Workout Marker');
+	}
+
+	// Render Workout
+	_renderWorkout() {
+		console.log('Render Workout');
+	}
+
+	// Move to Popup
+	_moveToPopup() {
+		console.log('Move to Popup');
+	}
+
+	// Set Local Storage
+	_setLocalStorage() {
+		console.log('Set Local Storage');
+	}
+
+	// Get Local Storage
+	_getLocalStorage() {
+		console.log('Get Local Storage');
+	}
+
+	// Reset
+	reset() {
+		console.log('Reset');
+	}
+
+
+}
 
 if (navigator.geolocation) {
 	navigator
@@ -53,7 +121,7 @@ if (navigator.geolocation) {
 			//  in the form submit event listener below because we
 			//  need access to latitude and longitude
 			//	Todo: Extract position on the map on click
-			map.on('click', function(mapE) {
+			map.on('click', function (mapE) {
 				mapEvent = mapE;
 				form.classList.remove('hidden');
 				inputDistance.focus();
@@ -73,22 +141,17 @@ form.addEventListener('submit', function (e) {
 
 
 	// Display marker
-	const { lat, lng } = mapEvent.latlng;
+	const {lat, lng} = mapEvent.latlng;
 	L.marker([lat, lng])
 		.addTo(map)
-		.bindPopup(
-			L.popup({
-				maxWidth: 250,
-				minWidth: 100,
-				autoClose: false,
-				closeOnClick: false,
-				className: 'running-popup'
+		.bindPopup(L.popup({
+			maxWidth: 250, minWidth: 100, autoClose: false, closeOnClick: false, className: 'running-popup'
 		}))
 		.setPopupContent('Workout')
 		.openPopup();
 })
 
-inputType.addEventListener('change', function(e) {
+inputType.addEventListener('change', function (e) {
 	inputElevation
 		.closest('.form__row')
 		.classList.toggle('form__row--hidden');
