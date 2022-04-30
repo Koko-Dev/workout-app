1231720a1227e8c4b2bd7630c7a05f26db037022
Koko-Dev <koko.webdev@gmail.com>
2022-04-30 17:34:46 -0400

    In script.js, get coordinates when page loads by calling the _getPosition() method within the constructor; at this point the app should work again

--- js/script.js
+++ js/script.js
@@ -11,27 +11,12 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-
-/*
- Note:  Geolocation.getCurrentPosition syntax
- The Geolocation.getCurrentPosition() method is used to get
- the current position of the device
- code:
- getCurrentPosition(success)
- getCurrentPosition(success, error)
- getCurrentPosition(success, error, options)
-
- Note: From google maps: https://www.google.com/maps/place/Virginia/@37.9820815,-81.6639165,7z/data=!3m1!4b1!4m5!3m4!1s0x884cd670bdbcb2cd:0xc04e4149b746a695!8m2!3d37.4315734!4d-78.6568942
-
- https://www.google.com/maps/@37.0722523,-76.3753041,15z
- */
-
-// Todo:  Reassign leaflet's map variable  and the mapEvent to the global scope
 let map, mapEvent;
 
 class App {
 	constructor() {
-
+		// Get coordinates when page loads
+		this._getPosition();
 	}
 
 	// Get Position
@@ -121,6 +106,8 @@ class App {
 
 }
 
+//  Create an instance of class App
+const app = new App();
 
 
 //  Todo: Add an event listener which displays a marker whenever
