60da95f4baec76582a1ec830482f5401e77fd900
Koko-Dev <koko.webdev@gmail.com>
2022-05-02 14:53:45 -0400

    In script.js, in child class Cycling, call method this.calcSpeed() in the constructor
    class Cycling extends Workout {
        constructor(coords, distance, duration, elevationGain) {
            super(coords, distance, duration);
            this.elevationGain = elevationGain;
            this.calcSpeed();
        }
    
        calcSpeed() {
            // calculated in hours so must divide by 60
            this.speed = this.distance / (this.duration / 60);
            return this.speed;
        }
    }

--- js/script.js
+++ js/script.js
@@ -41,8 +41,9 @@ class Cycling extends Workout {
 	constructor(coords, distance, duration, elevationGain) {
 		super(coords, distance, duration);
 		this.elevationGain = elevationGain;
+		this.calcSpeed();
 	}
-	
+
 	calcSpeed() {
 		// calculated in hours so must divide by 60
 		this.speed = this.distance / (this.duration / 60);
