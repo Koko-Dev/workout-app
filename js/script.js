99b3559d6b4d2ec0c6dc4450cf9aa018bed33180
Koko-Dev <koko.webdev@gmail.com>
2022-05-02 14:38:21 -0400

    In script.js, in class Workout, create fields for current date and unique identifier (this is for testing so we will not use a library) based on the last ten numbers of the current date

--- js/script.js
+++ js/script.js
@@ -12,6 +12,9 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 class Workout {
+	date = new Date();
+	id = (new Date() + '').slice(-10);
+	
 	constructor(coords, distance, duration) {
 		this.coords = coords;
 		this.distance = distance;  //km
